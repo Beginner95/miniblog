6a6756bc0c06d417438e1580d85ab4f569480d97
Vaharsolta <vaha-22@mail.ru>
2019-10-07 22:54:56 +0300

    We receive data from the form

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,6 +1,25 @@
 import { Component } from '../core/component'
+import { Form } from '../core/form'
+
 export class CreateComponent extends Component {
     constructor(id) {
         super(id)
+    }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this))
+        this.form = new Form(this.$el, {
+            title: [],
+            fulltext: []
+        })
+
+    }
+}
+
+function submitHandler(event) {
+    event.preventDefault()
+    
+    const formData = {
+        type: this.$el.type.value
     }
 }
