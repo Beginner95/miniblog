1b3f41d662bda440d15d2cd3f1120baa20a688b3
Vaharsolta <vaha-22@mail.ru>
2019-10-09 11:53:41 +0300

    Implemented receiving posts from the database

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -9,14 +9,27 @@ class ApiService {
                 method: 'post',
                 body: JSON.stringify(post)
             })
-            const response = await fetch(request)
-            return await response.json()
+            return useRequest(request)
         } catch(error) {
             console.error(error)
         }
-
-        
     }
+
+    async fetchPosts() {
+        try {
+            const request = new Request(`${this.url}/posts.json`, {
+                method: 'get'
+            })
+            return useRequest(request)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+async function useRequest(request) {
+    const response = await fetch(request)
+    return await response.json()
 }
 
 export const apiService = new ApiService('https://js-native-blog-43f9a.firebaseio.com/')
