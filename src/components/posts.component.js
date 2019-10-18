641e4c9114e786742caa80bd1d620c0599466532
Vaharsolta <vaha-22@mail.ru>
2019-10-18 14:15:41 +0300

    Implemented adding post in favorites

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -7,6 +7,10 @@ export class PostsComponent extends Component {
         super(id)
     }
 
+    init() {
+        this.$el.addEventListener('click', buttonHendler.bind(this))
+    }
+
     async onShow() {
         const fbData = await apiService.fetchPosts()
         const posts = TransformService.fbObjectToArray(fbData)
@@ -24,8 +28,10 @@ function renderPost(post) {
         ? '<li class="tag tag-blue tag-rounded">News</li>'
         : '<li class="tag tag-rounded">Note</li>'
 
-    const button = '<button class="button-round button-small button-primary">Save</button>'
-        
+    const button = (JSON.parse(localStorage.getItem('favorites')) || []).includes(post.id)
+        ? `<button class="button-round button-small button-danger" data-id=${post.id}>Delete</button>`
+        : `<button class="button-round button-small button-primary" data-id=${post.id}>Save</button>`
+
     return `
         <div class="panel">
             <div class="panel-head">
@@ -43,4 +49,27 @@ function renderPost(post) {
             </div>
         </div>
     `
+}
+
+function buttonHendler(event) {
+    const $el = event.target
+    const id = $el.dataset.id
+
+    if (id) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || []
+
+        if (favorites.includes(id)) {
+            $el.textContent = 'Save'
+            $el.classList.add('button-primary')
+            $el.classList.remove('button-danger')
+            favorites = favorites.filter(fId => fId !== id)
+        } else {
+            $el.textContent = 'Delete'
+            $el.classList.add('button-danger')
+            $el.classList.remove('button-primary')
+            favorites.push(id)
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }
 }
